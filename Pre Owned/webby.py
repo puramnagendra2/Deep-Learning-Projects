eaf5cd665ff29c2b8dc44aede94c40bf55c688b6
puramnagendra2 <puramnagendra2@gmail.com>
2025-01-26 23:29:09 +0530

    Added Opening Page

--- Pre Owned/webby.py
+++ Pre Owned/webby.py
@@ -9,9 +9,13 @@ app = Flask(__name__)
 model = joblib.load('price_prediction_pipeline.h5')
 
 @app.route('/')
+def open():
+    return render_template('open.html')
+
+
+@app.route('/index')
 def index():
     return render_template('index.html')
-
 
 @app.route('/predict', methods=['POST'])
 def predict():
